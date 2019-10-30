262043ece7b5b0f858889c7ed8a5026ec7220d79
Kim Jonghyeok <proje_ct@naver.com>
2019-10-31 01:04:18 +0900

    Study React-Redux(2)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { createStore } from 'redux';
-import Counter from './components/Counter';
 
 const INCREMENT = "INCREMENT";
 
@@ -13,36 +10,41 @@ function increase(diff) {
     };
 }
 
-const initialState = {
-  value: 0
-}
-
-const counterReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case INCREMENT:
-      return Object.assign({}, state, {
-        value: state.value + action.addBy
-      })
-      default:
-        return state;
-  }
-}
-
-
-const store = createStore(counterReducer);
 class App extends Component {
-
-  clickTest = () => {
-    alert("테스트");
-    store.dispatch(increase(1));
+  constructor(props) {
+      super(props);
+      this.onClick = this.onClick.bind(this);
   }
 
   render() {
-    return (
-      <Counter store={store} click={() => this.clickTest()}/>
-    )
+
+      let centerStyle = {
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          WebkitUserSelect: 'none',
+          MozUserSelect: 'none',
+          MsUserSelect:'none',
+          userSelect: 'none',
+          cursor: 'pointer'
+      };
+
+      return (
+          <div
+              onClick={ this.onClick }
+              style={ centerStyle }
+          >
+
+              <h1> {this.props.store.getState().value} </h1>
+              <h1> {this.props.store.getState().start} </h1>
+          </div>
+      )
+  }
+
+  onClick() {
+      this.props.store.dispatch(increase(1));
   }
 }
 
-store.subscribe(App);
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
+const initialState = {
+  value: 0,
+  start: 0
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counterReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return Object.assign({}, state, {
+        value: state.value + action.addBy,
+        start: state.start + action.addBy + 5
+      })
+      default:
+        return state;
+  }
+}
+/*
+ * Store
+ */
+const store = createStore(counterReducer);
+const render = () => {
+    const appElement = document.getElementById('root');
+    ReactDOM.render(<App store={store} />, appElement);
+};
 
+store.subscribe(render);
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
